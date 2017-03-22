da2a5cc2b9af0c43a17d588a42a53c24de757d4f
wangjf <wangjf55555@163.com>
2017-03-22 20:35:29 +0800

    settings页面上传图片插件---未完成版

--- public/js/settings.js
+++ public/js/settings.js
@@ -2,15 +2,32 @@
  * Created by Administrator on 2017/3/22.
  */
 
-define(['jquery','template','ckeditor','region','validate','form','datepicker','language','uploadify'],function ($,template,CKEDITOR) {
+define(['jquery', 'template', 'ckeditor', 'region', 'validate', 'form', 'datepicker', 'language', 'uploadify'], function ($, template, CKEDITOR, uploadify) {
     $.ajax({
-        type:'get',
-        url:'/api/teacher/profile',
-        dataType:'json',
-        success:function (data) {
+        type: 'get',
+        url: '/api/teacher/profile',
+        dataType: 'json',
+        success: function (data) {
             console.log(data);
-            var html=template('settingsTpl',data.result);
+            var html = template('settingsTpl', data.result);
             $('#putSettingsTpl').html(html);
+
+            // 执行图片上传
+            $('#upfile').uploadify({
+                swf: '/public/assets/uploadify/uploadify.swf',
+                width:120,
+                height:120,
+                type:'post',
+                buttonText: '',
+                fileObjName: 'tc_avatar',
+                data:{tc_avatar:'tc_avator'},
+                uploader: '/api/uploader/avatar',
+                onUploadSuccess : function(file,data){
+                    console.log(data);  //???????????????????????
+                }
+            })
+
+
         }
     })
 })
